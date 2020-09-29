36f1f292e9111526e723ef38350968afc5cea420
unknown <jost.gregor@gmail.com>
2020-09-29 18:09:29 +0200

    hello circular slider

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  entry: "./src/circular-slider.js",
+  entry: {
+    circularSlider: "./src/circular-slider.js",
+  },
   devServer: {
     contentBase: "./dist",
+    writeToDisk: true,
     inline: true,
     hot: true,
   },
   output: {
-    filename: "circular-slider.bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My circular slider",
+      template: "assets/index.html",
+    }),
+  ],
 };
