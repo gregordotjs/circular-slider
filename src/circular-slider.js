cc6f24edd6e37bc7ccb71026231dcf6da5be90e6
unknown <jost.gregor@gmail.com>
2020-10-01 21:46:04 +0200

    Supports on click on circle, snaps to the closest position

--- src/circular-slider.js
+++ src/circular-slider.js
@@ -35,8 +35,12 @@ export class CircularSlider {
   #circumference = 0;
 
   #isMouseDown = false;
+  #lastMousePosition = {
+    x: null,
+    y: null,
+  };
 
-  /** @type {(value: string) => void | null} */
+  /** @type {(value: number) => void | null} */
   #handleChange = null;
 
   /**
@@ -60,7 +64,7 @@ export class CircularSlider {
   #sliderHandle = null;
 
   /**
-   * @param {{ container: string; color: string; max: number; step: number; radius: number; min?: number; onChange?: (value: string) => void; }} options
+   * @param {{ container: string; color: string; max: number; step: number; radius: number; min?: number; onChange?: (value: number) => void; }} options
    */
   constructor(options) {
     const {
@@ -140,7 +144,7 @@ export class CircularSlider {
         ...circleAttributes,
         ["stroke-dasharray"]: this.#circumference,
         ["stroke-dashoffset"]: this.#circumference,
-        style: `transform: rotate(-90deg); transform-origin: ${x}px ${y}px; transition: stroke-dashoffset 0.40s;`,
+        style: `transform: rotate(-90deg); transform-origin: ${x}px ${y}px; transition: stroke-dashoffset 0.40s; cursor: pointer;`,
         stroke: this.#color,
       },
       CIRCLE
@@ -165,49 +169,44 @@ export class CircularSlider {
     this.#SVGContainer.appendChild(this.#sliderHandle);
 
     // Setting up events for SVG elements
-    this.#circle.addEventListener(MOUSE_UP, this.#onMouseDown);
-    this.#circle.addEventListener(CLICK, this.#onMouseClick);
-    this.#progressCircle.addEventListener(CLICK, this.#onMouseClick);
-
     this.#sliderHandle.addEventListener(MOUSE_DOWN, this.#onMouseDown);
-    this.#sliderHandle.addEventListener(MOUSE_UP, this.#onMouseDown);
     this.#sliderHandle.addEventListener(MOUSE_MOVE, this.#onMouseMove);
+    this.#sliderHandle.addEventListener(MOUSE_UP, this.#onClick);
+    this.#circle.addEventListener(CLICK, this.#onClick);
+    this.#progressCircle.addEventListener(CLICK, this.#onClick);
   };
 
   /**
    * @param {MouseEvent} e
    */
-  #onMouseClick = (e) => {
-    this.#isMouseDown = true;
-    this.#onMouseMove(e);
+  #onClick = (e) => {
+    this.#sliderHandle.setAttribute("style", "cursor: grab");
     this.#isMouseDown = false;
-  };
-
-  #onMouseDown = ({ type }) => {
-    switch (type) {
-      case MOUSE_DOWN:
-        this.#sliderHandle.setAttribute("style", "cursor: grabbing");
-        this.#isMouseDown = true;
-        break;
-
-      default:
-        this.#sliderHandle.setAttribute("style", "cursor: grab");
-        this.#isMouseDown = false;
-        break;
-    }
+    this.#slide(e);
   };
 
   /**
-   *
+   * @param {MouseEvent} e
+   */
+  #onMouseDown = (e) => {
+    this.#sliderHandle.setAttribute("style", "cursor: grabbing");
+    this.#isMouseDown = true;
+  };
+
+  /**
+   * @param {MouseEvent} e
+   */
+  #onMouseUp = (e) => {};
+
+  /**
    * @param {MouseEvent} e
    */
   #onMouseMove = (e) => {
     if (!this.#isMouseDown) return;
-    const { pageX, pageY } = e;
+    this.#slide(e);
+  };
 
-    // Calculating new position with e.x and e.y (SVG coordinates) doesn't yield the correct behavior (slider handle is always a bit off).
-    // Obtaining screen coordinates from SVG coordinates
-
+  #slide = ({ pageX, pageY }) => {
     // @ts-ignore
     let pt = this.#SVGContainer.createSVGPoint();
 
@@ -218,10 +217,10 @@ export class CircularSlider {
     // @ts-ignore
     pt = pt.matrixTransform(this.#SVGContainer.getScreenCTM().inverse());
 
-    const { x, y, radians, degrees, percentage } = this.#calculateCoordinates(
-      pt.x,
-      pt.y
-    );
+    this.#lastMousePosition.x = pt.x;
+    this.#lastMousePosition.y = pt.y;
+
+    const { x, y, percentage } = this.#calculateCoordinates(pt.x, pt.y);
 
     this.#sliderHandle.setAttribute("cx", x.toString());
     this.#sliderHandle.setAttribute("cy", y.toString());
@@ -232,7 +231,7 @@ export class CircularSlider {
    *
    * @param {number} x
    * @param {number} y
-   * @returns {{x: number, y: number, radians: number, degrees: number, percentage: number}}
+   * @returns {{x: number, y: number, percentage: number}}
    */
   #calculateCoordinates = (x, y) => {
     // atan2 returns the angle in radians: https://math.stackexchange.com/questions/94379/calculating-angle-in-circle
@@ -244,21 +243,38 @@ export class CircularSlider {
 
     // calculate the number of steps
     const steps = (this.#max - this.#min) / this.#step;
-    
+
     // get a single step in degrees
     const stepInDegrees = 360 / steps;
 
     let startDegrees = 0;
-    let index = 0;
+    let index = 1;
 
     // go through all possible steps and identify where the current mouse position is
     for (let i = 1; i <= steps; i++) {
       let min = startDegrees;
       let max = startDegrees + stepInDegrees;
-      if (degrees >= min && degrees <= max) {
-        index++;
-        this.#handleChange &&
-          this.#handleChange((this.#min + this.#step * index).toString());
+
+      // introduced a divider; less than the divider is min value, more is max
+      const divider = (min + max) / 2;
+
+      if (degrees >= min && degrees <= divider) {
+        // this will force the progress circle to "snap back" if degrees are less than the divider
+        index--;
+
+        // this will force the slide handler to "snap" to the nearest step
+        if (!this.#isMouseDown) {
+          radians = (min - tilt) * (Math.PI / 180);
+        }
+        this.#onChange(this.#min + this.#step * index);
+        break;
+      }
+
+      if (degrees > min && degrees > divider && degrees < max) {
+        if (!this.#isMouseDown) {
+          radians = (max - tilt) * (Math.PI / 180);
+        }
+        this.#onChange(this.#min + this.#step * index);
         break;
       }
       index++;
@@ -267,8 +283,6 @@ export class CircularSlider {
 
     // calculate new coordinates based on angle: https://en.wikipedia.org/wiki/Circle#Equations
     return {
-      radians,
-      degrees,
       percentage: index / steps,
       x: defaults.center.x + this.#radius * Math.cos(radians),
       y: defaults.center.y + this.#radius * Math.sin(radians),
@@ -279,8 +293,16 @@ export class CircularSlider {
    * @param {number} percentage
    */
   #moveProgressCircle = (percentage) => {
-    console.log({ percentage });
     let offset = this.#circumference - percentage * this.#circumference;
     this.#progressCircle.setAttribute("stroke-dashoffset", offset.toString());
+  };
+
+  /**
+   * @param {number} value
+   */
+  #onChange = (value) => {
+    if (this.#handleChange !== null) {
+      this.#handleChange(value);
+    }
   };
 }
